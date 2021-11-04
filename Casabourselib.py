e7c8837327e184d036215f6edcb450a496e6ba84
Yassine Rhzif <Rhzif@hotmail.com>
2021-11-04 23:03:00 +0100

    some modifications ...

--- Casabourselib.py
+++ Casabourselib.py
@@ -7,10 +7,10 @@ from bs4 import BeautifulSoup
 import html5lib
 import random
 
-def get_price(ticker, from_date, to_date):
+def get_stock_price(ticker, from_date, to_date):
   from_date = from_date[6:]+'-'+from_date[3:5]+'-'+from_date[0:2]
   to_date = to_date[6:]+'-'+to_date[3:5]+'-'+to_date[0:2]
-  ticker_2_ISIN =  { "ADH": "MA0000011512", "AFM": "MA0000012296", "AFI": "MA0000012114", "GAZ": "MA0000010951", "AGM": "MA0000010944", "ADI": "MA0000011819", "ALM": "MA0000010936",
+  data =  { "ADH": "MA0000011512", "AFM": "MA0000012296", "AFI": "MA0000012114", "GAZ": "MA0000010951", "AGM": "MA0000010944", "ADI": "MA0000011819", "ALM": "MA0000010936",
  "ARD": "MA0000012460", "ATL": "MA0000011710", "ATW": "MA0000012445", "ATH": "MA0000010969", "NEJ": "MA0000011009", "BAL": "MA0000011991", "BOA": "MA0000012437",
  "BCP": "MA0000011884", "BCI": "MA0000010811", "CRS": "MA0000011868", "CDM": "MA0000010381", "CDA": "MA0000012049", "CIH": "MA0000011454", "CMA": "MA0000010506",
  "CMT": "MA0000011793", "COL": "MA0000011934", "CSR": "MA0000012247", "CTM": "MA0000010340", "DRI": "MA0000011421", "DLM": "MA0000011777", "DHO": "MA0000011850",
@@ -23,11 +23,30 @@ def get_price(ticker, from_date, to_date):
  "SNP": "MA0000011728", "MSA": "MA0000012312", "SID": "MA0000010019", "SOT": "MA0000012502", "SRM": "MA0000011595", "SBM": "MA0000010365", "STR": "MA0000012056",
  "TQM": "MA0000012205", "TIM": "MA0000011686", "TMA": "MA0000012262", "UMR": "MA0000012023", "WAA": "MA0000010928", "ZDJ": "MA0000010571"  }
 
-  url='https://www.leboursier.ma/api?method=getPriceHistory&ISIN='+str(ticker_2_ISIN[ticker])+'&format=json&from='+str(from_date)+'&to='+str(to_date)
+  url='https://www.leboursier.ma/api?method=getPriceHistory&ISIN='+str(data[ticker])+'&format=json&from='+str(from_date)+'&to='+str(to_date)
   r = requests.get(url)
   data = json.loads(codecs.decode(r.text.encode(), 'utf-8-sig'))
   df = pd.json_normalize(data['result'])
-  return df 
+  df = df.set_index('date')
+  return df
+
+
+def get_masi(periode):
+  if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+  from pandas.core.common import SettingWithCopyWarning
+  warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+  # par exemple periode = '1m' , '1y' , '5y'
+  url='https://www.leboursier.ma/api?method=getMasiHistory&periode='+str(periode)+'&format=json'
+  r = requests.get(url)
+  data = json.loads(codecs.decode(r.text.encode(), 'utf-8-sig'))
+  df1 = pd.json_normalize(data['result'])
+  df = pd.DataFrame(zip(df1.labels[0], df1.prices[0]),columns =['date','value'])
+  for i in range(len(df.date)):
+    df.date[i]= datetime.fromtimestamp(df.date[i]).strftime('%d/%m/%Y')
+  df = df.set_index('date')
+  return df
+
 
 
 def get_indicators(ticker):
